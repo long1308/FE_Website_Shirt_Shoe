33a9e84f88f2a19f90a2e49befd18cb0c8430d17
Lehung1231 <hungltph22978@fpt.edu.vn>
2023-07-10 21:07:00 +0700

    feat:WB-24/[FE]-Style css Banner,NotFound

--- src/pages/HomePages.tsx
+++ src/pages/HomePages.tsx
@@ -1,10 +1,12 @@
-import { BannerHome } from "../components";
+import { BannerCaption, BannerCategory, BannerHome } from "../components";
 import Container from "./Container";
 const HomePages = () => {
     return (
         <div>
             <BannerHome></BannerHome>
+            <BannerCategory></BannerCategory>
             <Container />
+            <BannerCaption></BannerCaption>
         </div>
     );
 };
