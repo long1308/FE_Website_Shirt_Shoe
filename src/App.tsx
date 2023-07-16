f480c7c2b2e32edae1fde3d5c1e19b9b42b4a403
Lehung1231 <hungltph22978@fpt.edu.vn>
2023-07-16 21:13:38 +0700

    feat:WB-38/[FE]-Style css AddColor

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,6 @@ import ProductAdd from './components/Admin/ProductAdd';
 import Customers from './pages/Admin/Custo/Customers'
 import ProductEdit from './components/Admin/ProductEdit';
 import ColorAdd from './components/Admin/Color/ColorAdd';
-import ColorEdit from './components/Admin/Color/ColorEdit';
-import ColorList from './components/Admin/Color/ColorList';
 function App() {
   // const dispatch = useDispatch();
   // const [isProductsLoaded, setProductsLoaded] = useState(false);
@@ -53,8 +51,6 @@ function App() {
           <Route path="customers" element={<Customers />} />
           <Route path="products" element={<ListProduct />} />
           <Route path="products/colors/add" element={<ColorAdd />} />
-          <Route path="products/colors/:id" element={<ColorEdit />} />
-          <Route path="products/colors" element={<ColorList />} />
           {/* <Route
             path="products"
             element={
